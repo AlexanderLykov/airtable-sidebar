c1f62dd38ac166fcce1113a53194ca1e1e50a911
Alex Lykov <alex.lykov.dev@gmail.com>
2021-06-10 13:55:32 +0300

    create sidebar

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,70 @@
-import {initializeBlock} from '@airtable/blocks/ui';
-import React from 'react';
+import React from "react";
+import {
+  Box,
+  Heading,
+  initializeBlock,
+  useBase,
+  useRecords,
+  useViewMetadata,
+  useWatchable,
+  CellRenderer,
+  Label,
+  useCursor,
+} from "@airtable/blocks/ui";
 
-function HelloWorldApp() {
-    // YOUR CODE GOES HERE
-    return <div>Hello world 🚀</div>;
+function TableStructureApp() {
+  const base = useBase();
+  const cursor = useCursor();
+  useWatchable(cursor, ["activeTableId", "activeViewId"]);
+
+  const table = base.getTableByIdIfExists(cursor.activeTableId);
+  const view = table && table.getViewByIdIfExists(cursor.activeViewId);
+
+  if (table && view) {
+    return <TableSchema base={base} table={table} view={view} />;
+  } else {
+    return null;
+  }
 }
 
-initializeBlock(() => <HelloWorldApp />);
+function TableSchema({ base, table, view }) {
+  const viewMetadata = useViewMetadata(view);
+  const recordsQueryResult = view.selectRecords();
+  const records = useRecords(recordsQueryResult);
+
+  return (
+    <Box>
+      <Box padding={3} borderBottom="thick">
+        <Heading size="small" margin={0}>
+          {table.name} / {view.name}
+        </Heading>
+      </Box>
+      {records.map((record) => (
+        <Box key={record._id} padding={3} borderBottom="thick">
+          {viewMetadata.visibleFields.map((field) => {
+            return (
+              <FieldRow
+                base={base}
+                table={table}
+                record={record}
+                field={field}
+                key={field.id}
+              />
+            );
+          })}
+        </Box>
+      ))}
+    </Box>
+  );
+}
+
+function FieldRow({ field, record }) {
+  return (
+    <Box marginTop={"16px"} flex="none" paddingRight={1}>
+      <Label fontWeight="strong">{field.name}</Label>
+      <CellRenderer field={field} record={record} />
+    </Box>
+  );
+}
+
+initializeBlock(() => <TableStructureApp />);
